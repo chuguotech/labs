9477f3f2198385ad21684db5d149ea3194992356
Hawk Tian <itonvin@gmail.com>
2024-04-09 10:21:05 +0800

    php code decode json data from http request body

---
+++ www/php/json_decode.php
@@ -0,0 +1,12 @@
+<?php
+$http_json_data = null;
+$http_json_data = file_get_contents('php://input');
+if ( !empty($http_json_data) ) {
+	$data = json_decode($http_json_data, true);
+	if ( json_last_error() !== JSON_ERROR_NONE ) {
+		if ( is_array($data) ) {
+			var_dump($data);
+		}
+	}
+
+}
